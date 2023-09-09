6f7b45e52ff8e0c8dd6e1776ac9bef5ce0b1300e
Sebastian Bernachia <sebastian.bernachia@outlook.com>
2023-09-08 21:48:58 -0300

    Primer commit

---
+++ scripts/carrusel.js
@@ -0,0 +1,18 @@
+let imgConteiner = document.querySelector(".banners-container");
+let puntos = document.querySelectorAll(".btn-banner");
+
+puntos.forEach((cadaPunto, i) => {
+    puntos[i].addEventListener("click", () => {
+        let desplazamiento = i * -(100/3);
+
+        console.log(imgConteiner.style.transform);
+        imgConteiner.style.transform = `translateX(${desplazamiento}%)`;     
+        
+        
+        puntos.forEach((cadaPunto, i) => {
+            puntos[i].classList.remove("btn-active");
+        });
+        puntos[i].classList.add("btn-active");
+    
+    });     
+});
